dd70fff48ce77392a4c7e87e367b758a572ab16f
Russeevich <kokanazar@yandex.ru>
2021-05-29 08:52:36 +0300

    create tags edit

--- src/admin/components/tag/tag.vue
+++ src/admin/components/tag/tag.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="tag">{{title}}</div>
+  <div v-if="!close" class="tag">{{title}}</div>
+  <div v-else class="tag closed">
+    <h4 class="tag__title">{{title}}</h4>
+    <button class="tag__close" v-bind="$listeners" @click="$emit('remove', id)">&times;</button>
+  </div>
 </template>
 
 <script>
@@ -8,6 +12,11 @@ export default {
     title: {
       type: String,
       default: ""
+    },
+    close: Boolean,
+    id: {
+      type: Number,
+      default: -1
     }
   }
 }
